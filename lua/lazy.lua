0d4b99e7b7104307d658d673a15e8c752d3be688
Michal Svorc <dev@michalsvorc.com>
2024-08-17 06:06:41 +0200

    chore(lazy): move config files

--- lua/lazy.lua
+++ lua/lazy.lua
@@ -19,7 +19,7 @@ vim.opt.rtp:prepend(lazypath)
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
-require("config.options")
+require("core.options")
 
 -- Setup lazy.nvim
 require("lazy").setup({
