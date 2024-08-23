423051393a9a026300596d4fca2404f4c339f459
Michal Svorc <dev@michalsvorc.com>
2024-08-23 18:11:16 +0200

    feat(mini-ai): initial configuration

---
+++ lua/plugins/mini-ai.lua
@@ -0,0 +1,58 @@
+--[[
+Copyright 2024 https://github.com/folke
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Source: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/coding.lua
+Changed:
+  - split to a separate module
+--]]
+
+-- https://github.com/echasnovski/mini.ai
+-- Extend and create `a`/`i` textobjects.
+return {
+  "echasnovski/mini.ai",
+  event = "VeryLazy",
+  opts = function()
+    local ai = require("mini.ai")
+    return {
+      n_lines = 500,
+      custom_textobjects = {
+        o = ai.gen_spec.treesitter({ -- code block
+          a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+          i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+        }),
+        f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
+        c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
+        t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
+        d = { "%f[%d]%d+" }, -- digits
+        e = { -- Word with case
+          { "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
+          "^().*()$",
+        },
+        i = LazyVim.mini.ai_indent, -- indent
+        g = LazyVim.mini.ai_buffer, -- buffer
+        u = ai.gen_spec.function_call(), -- u for "Usage"
+        U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
+      },
+    }
+  end,
+  config = function(_, opts)
+    require("mini.ai").setup(opts)
+    LazyVim.on_load("which-key.nvim", function()
+      vim.schedule(function()
+        LazyVim.mini.ai_whichkey(opts)
+      end)
+    end)
+  end,
+}
