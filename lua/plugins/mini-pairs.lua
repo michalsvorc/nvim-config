a1de8d6d36a60c9a4e0d6e357575b8df9a5eb71c
Michal Svorc <dev@michalsvorc.com>
2024-08-23 18:12:31 +0200

    feat(mini-pairs): initial configuration

---
+++ lua/plugins/mini-pairs.lua
@@ -0,0 +1,41 @@
+--[[
+Copyright 2024 https://github.com/folke
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Source: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/coding.lua
+Changed:
+  - split to a separate module
+--]]
+
+-- https://github.com/echasnovski/mini.pairs
+-- Minimal and fast autopairs.
+return {
+  "echasnovski/mini.pairs",
+  event = "VeryLazy",
+  opts = {
+    modes = { insert = true, command = true, terminal = false },
+    -- skip autopair when next character is one of these
+    skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+    -- skip autopair when the cursor is inside these treesitter nodes
+    skip_ts = { "string" },
+    -- skip autopair when next character is closing pair
+    -- and there are more closing pairs than opening pairs
+    skip_unbalanced = true,
+    -- better deal with markdown code blocks
+    markdown = true,
+  },
+  config = function(_, opts)
+    LazyVim.mini.pairs(opts)
+  end,
+}
