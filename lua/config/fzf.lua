00398a7068de626e5417402a303a8adbb3ca2f45
Michal Svorc <dev@michalsvorc.com>
2024-09-14 15:42:30 +0200

    refactor(fzf): extract winopts for small window

---
+++ lua/config/fzf.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+M.winopts = {
+  small_window = {
+    layout = "vertical",
+    -- height is based on vim.o.lines, with a minimum of 25 lines
+    height = math.max(math.floor(vim.o.lines * 0.5 + 0.5), 25),
+    -- width is based on the number of columns, with a minimum of 80 columns
+    width = math.max(math.floor(vim.o.columns * 0.35 + 0.5), 80),
+    preview = {
+      layout = "vertical",
+      hidden = "hidden",
+    },
+  },
+}
+
+return M
