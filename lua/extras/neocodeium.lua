7c8740bb629cc7d8a75471eb969980513465014e
Michal Svorc <dev@michalsvorc.com>
2024-09-21 17:20:00 +0200

    refactor(neocodeium): split config for cmp and neocodeium

--- lua/extras/neocodeium.lua
+++ lua/extras/neocodeium.lua
@@ -1,21 +1,19 @@
 -- https://github.com/monkoose/neocodeium
 -- AI completion powered by Codeium.
-local opts = {
-  silent = true,
-  filetypes = {
-    gitcommit = true,
-    gitrebase = false,
-    help = false,
-    ["."] = false,
-  },
-}
-
 return {
   {
     "monkoose/neocodeium",
     event = "LazyFile",
     version = false,
-    opts = opts,
+    opts = {
+      silent = true,
+      filetypes = {
+        gitcommit = true,
+        gitrebase = false,
+        help = false,
+        ["."] = false,
+      },
+    },
     -- stylua: ignore
     keys = {
       { "<A-f>", function() require("neocodeium").accept() end, mode = "i" },
@@ -25,6 +23,18 @@ return {
       { "<A-r>", function() require("neocodeium").cycle_or_complete(-1) end, mode = "i" },
       { "<A-c>", function() require("neocodeium").clear() end, mode = "i" },
     },
+    config = function(_, opts)
+      -- Using neocodeium alongside nvim-cmp
+      -- Source: https://github.com/monkoose/neocodeium
+      local cmp = require("cmp")
+
+      ---@diagnostic disable-next-line: undefined-field
+      require("neocodeium").setup(LazyVim.merge(opts, {
+        filter = function()
+          return not cmp.visible()
+        end,
+      }))
+    end,
   },
   {
     "hrsh7th/nvim-cmp",
@@ -34,9 +44,7 @@ return {
         autocomplete = false,
       },
     },
-    config = function(_, cmp_opts)
-      -- Using alongside nvim-cmp
-      -- Source: https://github.com/monkoose/neocodeium
+    config = function(_, opts)
       local cmp = require("cmp")
       local neocodeium = require("neocodeium")
 
@@ -45,14 +53,7 @@ return {
         neocodeium.clear()
       end)
 
-      ---@diagnostic disable-next-line: undefined-field
-      neocodeium.setup(LazyVim.merge(opts, {
-        filter = function()
-          return not cmp.visible()
-        end,
-      }))
-
-      cmp.setup(cmp_opts)
+      cmp.setup(opts)
     end,
   },
 }
