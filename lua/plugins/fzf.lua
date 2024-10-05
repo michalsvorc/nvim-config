17eb2e4a8f4c8cbf21ca578bbc965d6b4ec25c03
Michal Svorc <dev@michalsvorc.com>
2024-10-05 17:08:14 +0000

    fix(fzf): fixes picker display for some plugins in visual mode

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -18,6 +18,8 @@ Removed:
   - setting vim.g.lazyvim_picker
   - folke/todo-comments.nvim plugin
   - folke/trouble.nvim
+  - custom LazyVim option to configure vim.ui.select
+    fixes picker display for some plugins (CodeCompanion) in visual mode
   - keys:
     * Switch Buffer
 Changed:
@@ -26,6 +28,7 @@ Changed:
   - window dimensions
   - use live_grep_glob for grep commands
   - fzf layout
+  - disabled init function to configure vim.ui.select
   - keys:
     * grep actions
     * jumplist
@@ -152,37 +155,6 @@ return {
             ueberzug_scaler = "fit_contain",
           },
         },
-        -- Custom LazyVim option to configure vim.ui.select
-        ui_select = function(fzf_opts, items)
-          return vim.tbl_deep_extend("force", fzf_opts, {
-            prompt = " ",
-            winopts = {
-              title = " " .. vim.trim((fzf_opts.prompt or "Select"):gsub("%s*:%s*$", "")) .. " ",
-              title_pos = "center",
-            },
-          }, fzf_opts.kind == "codeaction" and {
-            winopts = {
-              layout = "vertical",
-              -- height is number of items minus 15 lines for the preview, with a max of 80% screen height
-              height = math.floor(math.min(vim.o.lines * 0.8 - 16, #items + 2) + 0.5) + 16,
-              width = 0.5,
-              preview = not vim.tbl_isempty(LazyVim.lsp.get_clients({ bufnr = 0, name = "vtsls" })) and {
-                layout = "vertical",
-                vertical = "down:15,border-top",
-                hidden = "hidden",
-              } or {
-                layout = "vertical",
-                vertical = "down:15,border-top",
-              },
-            },
-          } or {
-            winopts = {
-              width = 0.5,
-              -- height is number of items, with a max of 80% screen height
-              height = math.floor(math.min(vim.o.lines * 0.8, #items + 2) + 0.5),
-            },
-          })
-        end,
         winopts = {
           width = 1,
           height = 1,
@@ -223,16 +195,6 @@ return {
     end,
     config = function(_, opts)
       require("fzf-lua").setup(opts)
-    end,
-    init = function()
-      LazyVim.on_very_lazy(function()
-        vim.ui.select = function(...)
-          require("lazy").load({ plugins = { "fzf-lua" } })
-          local opts = LazyVim.opts("fzf-lua") or {}
-          require("fzf-lua").register_ui_select(opts.ui_select or nil)
-          return vim.ui.select(...)
-        end
-      end)
     end,
     -- stylua: ignore
     keys = {
