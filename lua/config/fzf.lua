4dedf17d1bcc543cd048b95e4133d4694f19919f
Michal Svorc <dev@michalsvorc.com>
2024-10-05 20:09:37 +0000

    feat(fzf): preview in predefined small window

--- lua/config/fzf.lua
+++ lua/config/fzf.lua
@@ -6,10 +6,10 @@ M.winopts = {
     -- height is based on vim.o.lines, with a minimum of 25 lines
     height = math.max(math.floor(vim.o.lines * 0.5 + 0.5), 25),
     -- width is based on the number of columns, with a minimum of 80 columns
-    width = math.max(math.floor(vim.o.columns * 0.35 + 0.5), 80),
+    width = math.max(math.floor(vim.o.columns * 0.5 + 0.5), 80),
     preview = {
       layout = "vertical",
-      hidden = "hidden",
+      vertical = "down:45%",
     },
   },
 }
