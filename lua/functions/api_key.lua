540e6b9fe55c57460c0faf93f916775541cf79b6
Michal Svorc <dev@michalsvorc.com>
2024-10-16 18:11:48 +0200

    fix(api_key): error handling

--- lua/functions/api_key.lua
+++ lua/functions/api_key.lua
@@ -8,14 +8,22 @@ M.get_api_key = function(encrypted_file_path)
   end
   file:close()
 
-  local handler = io.popen("gpg --quiet --batch --yes --decrypt " .. encrypted_file_path)
+  local handler = io.popen("gpg --quiet --batch --yes --decrypt " .. encrypted_file_path .. " 2>&1")
   if not handler then
-    print("Error. File containing API key could not be decrypted: " .. encrypted_file_path)
+    print("Error. Could not execute gpg command.")
     return nil
   end
-  local result = handler:read("*a")
-  handler:close()
-  return vim.trim(result)
+
+  local output = handler:read("*all")
+  local success, exit_reason, exit_code = handler:close()
+
+  if success and not output:match("decryption failed") and not output:match("No secret key") then
+    return vim.trim(output)
+  else
+    print("Error. File could not be decrypted: " .. encrypted_file_path)
+    print("Exit reason: " .. exit_reason .. ", Exit code: " .. exit_code)
+    return nil
+  end
 end
 
 return M
