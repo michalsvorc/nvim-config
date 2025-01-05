7342814a8f2933004c4dac41317c323fa79289de
Michal Svorc <dev@michalsvorc.com>
2025-01-05 17:41:57 +0100

    refactor(mini.icons): use init function from LazyVim

--- lua/plugins/mini-icons.lua
+++ lua/plugins/mini-icons.lua
@@ -1,10 +1,31 @@
+--[[
+Copyright 2024 https://github.com/folke
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Source: https://github.com/LazyVim/LazyVim/blob/v14.6.0/lua/lazyvim/plugins/ui.lua#L244
+
+Changed:
+  - split to a separate module
+Removed:
+  - lazy loading declaration, use defaults
+--]]
+
 -- https://github.com/echasnovski/mini.icons
 -- Icon provider.
 
 return {
   "echasnovski/mini.icons",
-  event = "VeryLazy",
-  version = false,
   opts = {
     file = {
       [".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
@@ -14,9 +35,10 @@ return {
       dotenv = { glyph = "", hl = "MiniIconsYellow" },
     },
   },
-  config = function(_, opts)
-    local MiniIcons = require("mini.icons")
-    MiniIcons.setup(opts)
-    MiniIcons.mock_nvim_web_devicons()
+  init = function()
+    package.preload["nvim-web-devicons"] = function()
+      require("mini.icons").mock_nvim_web_devicons()
+      return package.loaded["nvim-web-devicons"]
+    end
   end,
 }
