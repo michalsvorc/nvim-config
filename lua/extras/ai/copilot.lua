011ae437f3ac14ae3683cfc185cca3f934ea75fa
Michal Svorc <dev@michalsvorc.com>
2025-01-05 18:07:13 +0100

    feat(ai): disable auto_trigger for inline suggestions
    + completions engine integration

--- lua/extras/ai/copilot.lua
+++ lua/extras/ai/copilot.lua
@@ -2,16 +2,13 @@
 -- Fully featured & enhanced replacement for copilot.vim
 -- complete with API for interacting with Github Copilot.
 
+local cmp = require("modules.cmp")
+
 return {
   {
     "zbirenbaum/copilot.lua",
     version = false,
     event = "LazyFile",
-    keys = {
-      { "<LocalLeader>s", "<cmd>Copilot panel<cr>", mode = { "n", "v" }, desc = "Suggestions panel" },
-      { "<LocalLeader>S", "<cmd>set buflisted | Copilot toggle<cr>", mode = { "n", "v" }, desc = "Suggestions toggle" },
-      { "<LocalLeader>p", "<cmd>Copilot! attach<cr>", mode = { "n", "v" }, desc = "Copilot attach" },
-    },
     opts = {
       panel = {
         layout = {
@@ -21,7 +18,7 @@ return {
       },
       suggestion = {
         enabled = true,
-        auto_trigger = true,
+        auto_trigger = false,
         hide_during_completion = true,
         debounce = 25,
         -- stylua: ignore
@@ -29,8 +26,6 @@ return {
           accept =      "<C-i>",
           accept_line = "<C-l>",
           accept_word = "<C-h>",
-          next =        "<C-j>",
-          prev =        "<C-k>",
           dismiss =     "<C-c>",
         },
       },
@@ -42,12 +37,39 @@ return {
         ["."] = false,
       },
     },
+    keys = {
+      { "<LocalLeader>s", "<cmd>Copilot panel<cr>", mode = { "n", "v" }, desc = "Suggestions panel" },
+      { "<LocalLeader>S", "<cmd>set buflisted | Copilot toggle<cr>", mode = { "n", "v" }, desc = "Suggestions toggle" },
+      { "<LocalLeader>p", "<cmd>Copilot! attach<cr>", mode = { "n", "v" }, desc = "Copilot attach" },
+      {
+        "<C-j>",
+        function()
+          require("copilot.suggestion").next()
+          cmp.menu_hide()
+        end,
+        mode = "i",
+      },
+      {
+        "<C-k>",
+        function()
+          require("copilot.suggestion").prev()
+          cmp.menu_hide()
+        end,
+        mode = "i",
+      },
+    },
     config = function(_, opts)
       require("copilot").setup(opts)
 
       vim.api.nvim_set_hl(0, "CopilotSuggestion", { link = "Function" })
       vim.api.nvim_set_hl(0, "CopilotAnnotation", { link = "Function" })
 
+      vim.api.nvim_create_autocmd("User", {
+        pattern = cmp.events.menu_open,
+        callback = function()
+          require("copilot.suggestion").dismiss()
+        end,
+      })
     end,
   },
 }
--- lua/extras/ai/neocodeium.lua
+++ lua/extras/ai/neocodeium.lua
@@ -1,6 +1,8 @@
 -- https://github.com/monkoose/neocodeium
 -- AI completion powered by Codeium.
 
+local cmp = require("modules.cmp")
+
 return {
   {
     "monkoose/neocodeium",
@@ -8,6 +10,7 @@ return {
     event = "LazyFile",
     opts = {
       silent = true,
+      manual = true,
       filetypes = {
         gitcommit = true,
         gitrebase = false,
@@ -15,21 +18,39 @@ return {
         ["."] = false,
       },
     },
+
+
     -- stylua: ignore
     keys = {
       { "<C-i>", function() require("neocodeium").accept() end, mode = "i" },
       { "<C-l>", function() require("neocodeium").accept_line() end, mode = "i" },
       { "<C-h>", function() require("neocodeium").accept_word() end, mode = "i" },
-      { "<C-j>", function() require("neocodeium").cycle_or_complete() end, mode = "i" },
-      { "<C-k>", function() require("neocodeium").cycle_or_complete(-1) end, mode = "i" },
+      { "<C-j>",
+        function()
+          require("neocodeium").cycle_or_complete(1)
+          cmp.menu_hide()
+        end,
+        mode = "i"
+      },
+      {
+        "<C-k>",
+        function()
+          require("neocodeium").cycle_or_complete(-1)
+          cmp.menu_hide()
+        end,
+        mode = "i",
+      },
       { "<C-c>", function() require("neocodeium").clear() end, mode = "i" },
     },
-        end,
-      },
-    },
     config = function(_, opts)
-      local neocodeium = require("neocodeium")
+      require("neocodeium").setup(opts)
 
+      vim.api.nvim_create_autocmd("User", {
+        pattern = cmp.events.menu_open,
+        callback = function()
+          require("neocodeium").clear()
+        end,
+      })
     end,
   },
 }
