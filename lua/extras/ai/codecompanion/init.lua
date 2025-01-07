43736db4805249fa71a9377ba2627def1387f36d
Michal Svorc <dev@michalsvorc.com>
2025-01-07 11:05:08 +0100

    feat(codecompanion): add blink.cmp support
    https://github.com/olimorris/codecompanion.nvim/commit/a1166d5a2e9249f81060d328859382be57eae79d

--- lua/extras/ai/codecompanion/init.lua
+++ lua/extras/ai/codecompanion/init.lua
@@ -13,79 +13,93 @@ local get_api_key = require("modules.api_key").get_api_key
 vim.cmd([[cab cc CodeCompanion]])
 
 return {
-  "olimorris/codecompanion.nvim",
-  event = "VeryLazy",
-  dependencies = {
-    { "nvim-lua/plenary.nvim", branch = "master" }, -- https://github.com/olimorris/codecompanion.nvim/tree/v10.7.0?tab=readme-ov-file#package-installation see Pinned plugins
-    "nvim-treesitter/nvim-treesitter",
-    "echasnovski/mini.diff",
-  },
-  opts = {
-    adapters = {
-      openai = function()
-        if adapter ~= "openai" then
-          return {}
-        end
+  {
+    "olimorris/codecompanion.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- https://github.com/olimorris/codecompanion.nvim/tree/v10.7.0?tab=readme-ov-file#package-installation see Pinned plugins
+      "nvim-treesitter/nvim-treesitter",
+      "echasnovski/mini.diff",
+    },
+    opts = {
+      adapters = {
+        openai = function()
+          if adapter ~= "openai" then
+            return {}
+          end
 
-        local api_key = get_api_key(vim.g.api_key_openai_path)
-        local options = {
-          env = {
-            api_key = api_key,
-          },
-          schema = {
-            model = {
-              default = openai_model,
+          local api_key = get_api_key(vim.g.api_key_openai_path)
+          local options = {
+            env = {
+              api_key = api_key,
             },
-          },
-        }
+            schema = {
+              model = {
+                default = openai_model,
+              },
+            },
+          }
 
-        return require("codecompanion.adapters").extend(adapter, options)
-      end,
-    },
-    strategies = {
-      chat = {
-        adapter = adapter,
-        slash_commands = {
-          ["buffer"] = {
-            opts = {
-              provider = picker,
+          return require("codecompanion.adapters").extend(adapter, options)
+        end,
+      },
+      strategies = {
+        chat = {
+          adapter = adapter,
+          slash_commands = {
+            ["buffer"] = {
+              opts = {
+                provider = picker,
+              },
+            },
+            ["file"] = {
+              opts = {
+                provider = picker,
+              },
             },
           },
-          ["file"] = {
-            opts = {
-              provider = picker,
-            },
+        },
+        inline = {
+          adapter = adapter,
+        },
+      },
+      display = {
+        chat = {
+          start_in_insert_mode = true,
+          diff = {
+            provider = "mini_diff",
+          },
+          window = {
+            layout = "buffer", -- float|vertical|horizontal|buffer
           },
         },
       },
-      inline = {
-        adapter = adapter,
-      },
+      prompt_library = prompts,
     },
-    display = {
-      chat = {
-        start_in_insert_mode = true,
-        diff = {
-          provider = "mini_diff",
-        },
-        window = {
-          layout = "buffer", -- float|vertical|horizontal|buffer
-        },
-      },
+    -- stylua: ignore
+    keys = {
+      { "<LocalLeader>\\", "<cmd>CodeCompanionChat<cr>", mode = { "n", "v" }, desc = "Chat" },
+      { "<LocalLeader>v", "<cmd>vsplit | CodeCompanionChat Toggle<cr>", mode = { "n", "v" }, desc = "Chat (vsplit)" },
+      { "<LocalLeader>a", "<cmd>CodeCompanionActions<cr>", mode = { "n", "v" }, desc = "Actions" },
+      { "<LocalLeader>A", "<cmd>CodeCompanionChat Add<cr>", mode = "v", desc = "Add to Chat" },
+      { "<LocalLeader>c", function() require("codecompanion").prompt("commit") end, mode = "v", desc = "Commit message" },
+      { "<LocalLeader>d", function() require("codecompanion").prompt("lsp") end, mode = "v", desc = "Diagnostics" },
+      { "<LocalLeader>e", function() require("codecompanion").prompt("explain") end, mode = "v", desc = "Explain" },
+      { "<LocalLeader>f", function() require("codecompanion").prompt("fix") end, mode = "v", desc = "Fix" },
+      { "<LocalLeader>r", function() require("codecompanion").prompt("refactor") end, mode = "v", desc = "Refactor" },
+      { "<LocalLeader>t", function() require("codecompanion").prompt("tests") end, mode = "v", desc = "Unit tests" },
     },
-    prompt_library = prompts,
   },
-  -- stylua: ignore
-  keys = {
-    { "<LocalLeader>\\", "<cmd>CodeCompanionChat<cr>", mode = { "n", "v" }, desc = "Chat" },
-    { "<LocalLeader>v", "<cmd>vsplit | CodeCompanionChat Toggle<cr>", mode = { "n", "v" }, desc = "Chat (vsplit)" },
-    { "<LocalLeader>a", "<cmd>CodeCompanionActions<cr>", mode = { "n", "v" }, desc = "Actions" },
-    { "<LocalLeader>A", "<cmd>CodeCompanionChat Add<cr>", mode = "v", desc = "Add to Chat" },
-    { "<LocalLeader>c", function() require("codecompanion").prompt("commit") end, mode = "v", desc = "Commit message" },
-    { "<LocalLeader>d", function() require("codecompanion").prompt("lsp") end, mode = "v", desc = "Diagnostics" },
-    { "<LocalLeader>e", function() require("codecompanion").prompt("explain") end, mode = "v", desc = "Explain" },
-    { "<LocalLeader>f", function() require("codecompanion").prompt("fix") end, mode = "v", desc = "Fix" },
-    { "<LocalLeader>r", function() require("codecompanion").prompt("refactor") end, mode = "v", desc = "Refactor" },
-    { "<LocalLeader>t", function() require("codecompanion").prompt("tests") end, mode = "v", desc = "Unit tests" },
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    dependencies = {
+      "olimorris/codecompanion.nvim",
+    },
+    opts = {
+      sources = {
+        default = { "codecompanion" },
+      },
+    },
   },
 }
