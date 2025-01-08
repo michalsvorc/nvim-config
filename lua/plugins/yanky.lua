07754e739660ca073ee4e5641199a081be6b4965
Michal Svorc <dev@michalsvorc.com>
2025-01-08 16:35:14 +0100

    feat(plugins): add gbprod/yanky.nvim
    Improved Yank and Put functionalities for Neovim.

---
+++ lua/plugins/yanky.lua
@@ -0,0 +1,64 @@
+--[[
+Copyright 2024 https://github.com/folke
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Source: https://github.com/LazyVim/LazyVim/blob/v14.6.0/lua/lazyvim/plugins/extras/coding/yanky.lua
+
+Changed:
+  - ring storage
+  - history_length
+  - highlight duration
+  - keymaps
+Removed:
+  - recommended property
+  - telescope integration
+--]]
+
+-- https://github.com/gbprod/yanky.nvim
+-- Improved Yank and Put functionalities for Neovim.
+
+return {
+  "gbprod/yanky.nvim",
+  event = "LazyFile",
+  opts = {
+    ring = {
+      storage = "memory",
+      history_length = 15,
+    },
+    highlight = { timer = 250 },
+  },
+  keys = {
+    {
+      "<leader>p",
+      function()
+        vim.cmd([[YankyRingHistory]])
+      end,
+      mode = { "n", "x" },
+      desc = "Open Yank History",
+    },
+    -- stylua: ignore
+    { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank Text" },
+    { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put Text After Cursor" },
+    { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put Text Before Cursor" },
+    { "gp", "<Plug>(YankyGPutAfter)", mode = { "n", "x" }, desc = "Put Text After Selection" },
+    { "gP", "<Plug>(YankyGPutBefore)", mode = { "n", "x" }, desc = "Put Text Before Selection" },
+    -- navigation
+    { "[y", "<Plug>(YankyCycleForward)", desc = "Cycle Forward Through Yank History" },
+    { "]y", "<Plug>(YankyCycleBackward)", desc = "Cycle Backward Through Yank History" },
+    { "]p", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put Indented After Cursor (Linewise)" },
+    { "[p", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put Indented Before Cursor (Linewise)" },
+    { "]P", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put Indented After Cursor (Linewise)" },
+    { "[P", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put Indented Before Cursor (Linewise)" },
+  },
+}
