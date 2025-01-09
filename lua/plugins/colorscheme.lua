349e84fec02e0815eb65403fbd67a9f650c8c490
Michal Svorc <dev@michalsvorc.com>
2025-01-09 17:18:53 +0100

    feat(colorscheme): add plugin integrations

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,5 @@
 -- https://github.com/catppuccin/nvim
+-- https://github.com/catppuccin/nvim?tab=readme-ov-file#integrations
 
 return {
   "catppuccin/nvim",
@@ -13,5 +14,35 @@ return {
         Comment = { fg = colors.subtext0 },
       }
     end,
+    default_integrations = false,
+    integrations = {
+      aerial = true,
+      blink_cmp = true,
+      flash = true,
+      fzf = true,
+      grug_far = true,
+      mason = true,
+      mini = {
+        enabled = true,
+        indentscope_color = "text",
+      },
+      native_lsp = {
+        enabled = true,
+        virtual_text = {},
+        underlines = {
+          errors = { "underline" },
+          hints = { "underline" },
+          warnings = { "underline" },
+          information = { "underline" },
+        },
+        inlay_hints = {},
+      },
+      semantic_tokens = true,
+      snacks = true,
+      treesitter = true,
+      treesitter_context = true,
+      rainbow_delimiters = true,
+      which_key = true,
+    },
   },
 }
